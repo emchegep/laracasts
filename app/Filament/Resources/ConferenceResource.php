58e9d06003cb20b99898281b3567f1b13630ed18
pchege <emchegep@gmail.com>
2025-02-05 16:43:24 +0300

    added database schema using laravel blueshift

---
+++ app/Filament/Resources/ConferenceResource.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ConferenceResource\Pages;
+use App\Filament\Resources\ConferenceResource\RelationManagers;
+use App\Models\Conference;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ConferenceResource extends Resource
+{
+    protected static ?string $model = Conference::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Conference Name')
+                    ->default('My Conference')
+                    ->helperText("The name of the conference")
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\MarkdownEditor::make('description')
+                    ->required(),
+                Forms\Components\DatePicker::make('start_date')
+                    ->native(false)
+                    ->required(),
+                Forms\Components\DateTimePicker::make('end_date')
+                    ->native(false)
+                    ->required(),
+                Forms\Components\Toggle::make('end_date')
+                    ->default(true),
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'draft' => 'Draft',
+                        'published' => 'Published',
+                        'archived' => 'Archived',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('region')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('venue_id')
+                    ->relationship('venue', 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('start_date')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end_date')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('region')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('venue.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListConferences::route('/'),
+            'create' => Pages\CreateConference::route('/create'),
+            'edit' => Pages\EditConference::route('/{record}/edit'),
+        ];
+    }
+}
